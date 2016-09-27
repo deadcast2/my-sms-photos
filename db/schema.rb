0a8d6bd30e63f99826f48639e5605025c4742537
Caleb Cohoon <caleb@imap.cc>
2016-09-26 23:22:22 -0700

    Added photo model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160927045252) do
+ActiveRecord::Schema.define(version: 20160927062022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,12 @@ ActiveRecord::Schema.define(version: 20160927045252) do
     t.index ["number"], name: "index_phones_on_number", unique: true, using: :btree
   end
 
+  create_table "photos", force: :cascade do |t|
+    t.integer  "phone_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["phone_id"], name: "index_photos_on_phone_id", using: :btree
+  end
+
+  add_foreign_key "photos", "phones"
 end
