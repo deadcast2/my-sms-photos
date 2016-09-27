52c8d63366f210f3d095ddd0453088c7f68ccd76
Caleb Cohoon <caleb@imap.cc>
2016-09-26 23:50:40 -0700

    Added paperclip to photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,8 @@
 class Photo < ApplicationRecord
   belongs_to :phone
+  
+  has_attached_file :photo, styles: { medium: '300x300>', thumb: '150x150#' }
+    
+  validates_attachment :photo, presence: true,
+    content_type: { content_type: ['image/jpeg'] }
 end
