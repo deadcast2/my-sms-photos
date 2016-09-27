0577c65f875c0639d679f2eb17164dc49f605f10
Caleb Cohoon <caleb@imap.cc>
2016-09-27 13:37:03 -0700

    Adding host name to s3 config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,7 @@ Rails.application.configure do
     s3_storage_class: :REDUCED_REDUNDANCY,
     url: ':s3_domain_url',
     path: '/:class/:attachment/:id_partition/:style/:filename',
+    s3_host_name: 's3-website-us-west-2',
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
