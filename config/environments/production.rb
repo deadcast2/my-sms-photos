bfb74683105545cde7455b16efb21e4c7db451d5
Caleb Cohoon <caleb@imap.cc>
2016-09-27 13:52:42 -0700

    Updating s3 host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_storage_class: :REDUCED_REDUNDANCY,
-    s3_host_name: 's3-website-us-west-2',
+    s3_host_name: 's3-website-us-west-2.amazonaws.com',
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
