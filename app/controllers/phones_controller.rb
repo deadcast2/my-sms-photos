74063521d4cc512cd797cc0205ffea8e384c2f5f
Caleb Cohoon <caleb@imap.cc>
2016-09-27 17:59:53 -0700

    No need to add the +1 when wanting to view your photos

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,6 +1,6 @@
 class PhonesController < ApplicationController
   def index
-    if phone = Phone.find_by_number(params[:number])
+    if phone = Phone.find_by_number("+1#{params[:number]}")
       @photos = phone.photos
     else
       redirect_to '/404.html'
