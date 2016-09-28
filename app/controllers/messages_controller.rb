749afc4c571ed60f169303cbcff681629e48db6d
Caleb Cohoon <caleb@imap.cc>
2016-09-27 18:16:39 -0700

    Added some notifications

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,19 +2,15 @@ class MessagesController < ApplicationController
   protect_from_forgery except: :create
   
   def create
-    phone = Phone.find_or_create_by(number: params['From'])
+    phone = Phone.find_or_initialize_by(number: params['From'])
     
     params['NumMedia'].to_i.times do |index|
-      phone.photos.create({
+      phone.photos.build({
         photo_remote_url: params["MediaUrl#{index}"],
         body: params['Body']
       })
     end
     
-    Twilio::REST::Client.new.messages.create({
-      from: params['To'],
-      to: params['From'],
-      body: 'Hello!'
-    })
+    phone.save
   end
 end
--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,3 +1,25 @@
 class Phone < ApplicationRecord
+  after_create :send_welcome
+  after_save :confirmation
+  
   has_many :photos
+  
+  private
+  
+  def send_welcome
+    Twilio::REST::Client.new.messages.create({
+      from: '+12096617815',
+      to: self.number,
+      body: "Ahoy and welcome to My SMS Photos! You can access your photos 
+      by visting mysms.photos/#{self.number.sub('+1', '')}"
+    })
+  end
+  
+  def confirmation
+    Twilio::REST::Client.new.messages.create({
+      from: '+12096617815',
+      to: self.number,
+      body: 'We got your photos! :)'
+    })
+  end
 end
