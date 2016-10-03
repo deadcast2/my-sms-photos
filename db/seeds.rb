7caa06591c5955e00b5c62f008710ea48bb9868e
Caleb Cohoon <caleb@imap.cc>
2016-10-02 20:44:39 -0700

    removed action cable and added basic three js scene

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+phone = Phone.create({ number: "+12093427939" })
+
+3.times do |i|
+  phone.photos.create({
+    photo: File.new("#{Rails.root}/test/fixtures/files/image#{i}.jpeg"),
+    body: "image #{i}"
+  })
+end
