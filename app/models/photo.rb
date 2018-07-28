190ee5d75ad697a1e8c4c69be6e7627a26b585dc
deadcast <deadcast@sent.com>
2018-07-27 20:53:54 -0700

    Bumped up photo quality

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,15 +1,15 @@
 class Photo < ApplicationRecord
   belongs_to :phone
-  
-  has_attached_file :photo, 
+
+  has_attached_file :photo,
     processors: [:thumbnail, :paperclip_optimizer],
-    paperclip_optimizer: { jpegoptim: { allow_lossy: true, max_quality: 2 } },
+    paperclip_optimizer: { jpegoptim: { allow_lossy: true, max_quality: 9 } },
     styles: { thumb: '256x256#' }
   process_in_background :photo
-    
-  validates_attachment :photo, presence: true, content_type: 
+
+  validates_attachment :photo, presence: true, content_type:
     { content_type: ['image/jpeg'] }
-    
+
   def photo_remote_url= url
     self.photo = open(url, allow_redirections: :all)
   end
